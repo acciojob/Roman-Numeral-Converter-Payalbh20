cbe3b5c4e5b96d767bcd03090dd59f90b98d16ff
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-01-11 20:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
   //your code here
   let result = '';
 
@@ -24,13 +23,9 @@ function convertToRoman(num) {
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
 // console.log(convertToRoman(36));
 // console.log(convertToRoman(14)); // XIV
 // console.log(convertToRoman(798)); // DCCXCVIII
 // console.log(convertToRoman(3999)); //  MMMCMXCIX
-
-
-
 // do not edit below this line
 module.exports = convertToRoman
