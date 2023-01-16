5ca4d066df01999cfb9905f1289a86b396cf5b51
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-01-16 19:04:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 function convertToRoman(num) {
-  const obj = {
-    0:['M',1000], 
-    1:['D', 500], 
-    2:['C', 100], 
-    3:['L', 50], 
-    4:['X', 10], 
-    5:['V', 5], 
-    6:['I', 1]
-  };
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
 
-//your code here
-function convertToRoman(num) {
-if (num === 0) {
-  return '';
-}
-for (var i = 0; i < romanMatrix.length; i++) {
-  if (num >= romanMatrix[i][0]) {
-    return romanMatrix[i][1] + convertToRoman(num - romanMatrix[i][0]);
-  }
-}
-}
-
+  //your code here
+	var ans ='';
+	for(vae key in obj){
+	while(num>=obj[key]){
+		ans+= key;
+		num -= obj[key];
+	}
+	}
+	return ans;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
